551e1b33541f14197ec23fa23a646468427b26ba
Jageloo <jageloo.yadav@gmail.com>
2021-10-07 22:12:18 +0530

    initial import

---
+++ LoginScreenArchitecure/LoginScreenArchitecure/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginScreenArchitecure
+//
+//  Created by Jageloo Yadav on 07/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
